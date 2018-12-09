54b6a6e2d3d2c898eca65158b11eb3e697453f59
梨本雄太 <yuta.business01@gmail.com>
2018-12-09 23:00:27 +0900

    一旦コミット

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function() {
 	var $skillBar = $(".skillbar");
 	$skillBar.skillBars({
 		from: 0, // バーの動くスタート位置
-		speed: 4000, // 動くスピード
+		speed: 1500, // 動くスピード
 		interval: 100 // 動き始めるまでの時間
 	});
 });
