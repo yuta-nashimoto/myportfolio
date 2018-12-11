199c7c8049581544e4d1c7ab39f07ea701fc52d3
梨本雄太 <yuta.business01@gmail.com>
2018-12-12 00:15:43 +0900

    一旦コミット

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function() {
+	/***************************************************
+	 * スキルバー実装
+	****************************************************/
 	var $skillBar = $(".skillbar");
 	$skillBar.skillBars({
 		from: 0, // バーの動くスタート位置
@@ -6,7 +9,11 @@ $(function() {
 		interval: 100 // 動き始めるまでの時間
 	});
 
-	//画面幅でナビゲーションの表示切り替え
+
+
+	/***************************************************
+	 * ナビ表示の切り替え
+	****************************************************/
 	var nav = $('.gnav_wrap');
 	if (window.matchMedia('(min-width:768px)').matches) {
 		nav.removeClass('is_none');
@@ -14,13 +21,18 @@ $(function() {
 		nav.addClass('is_none');
 	}
 
+
+
+	/***************************************************
+	 * MVとメインタイトルのパララックスエフェクト
+	****************************************************/
 	var $window = $(window); //ウィンドウのDOMを取得
 	var targetBg = $('.parallax'); //背景のDOMを取得
 	var targetBgPos = targetBg.offset().top; //背景の位置を取得
 	var targetTxt = $('.parallax_txt'); //テキストのDOMを取得
 	var targetTxtPos = targetTxt.offset().top; //テキストの位置を取得
 	var targetFactor01 = 0.2;
-	var targetFactor02 = 1.3;
+	var targetFactor02 = 1.6;
 	var windowHeight = $window.height(); //画面の高さを取得
 	var scrollYStart01 = targetBgPos - windowHeight;
 	var scrollYStart02 = targetTxtPos - windowHeight;
@@ -37,4 +49,30 @@ $(function() {
 			targetTxt.css('top', '50%');
 		}
 	});
+
+
+	/***************************************************
+	 * スムーススクロール
+	****************************************************/
+	var $anchor = $('a[href^="#"]');
+	$anchor.click(function(){
+		var speed = 400;
+		var href = $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$('body,html').animate({scrollTop:position - 40}, speed, 'swing');
+		return false;
+	});
+
+	/***************************************************
+	 * portfolioエリア　オーバーレイ
+	****************************************************/
+	var $siteItem = $('.portfolio_item');
+	var $overRayItem = $('.portfolio_detail_box');
+	$siteItem.hover(function(){
+		$(this).find($overRayItem).stop().animate({'top': 0},200);
+	},function(){
+		$(this).find($overRayItem).stop().animate({'top': 100 + '%'},200);
+	});
+
 });
