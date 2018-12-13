c14bdca0aecb44fe0a619870228ea761bf904327
梨本雄太 <yuta.business01@gmail.com>
2018-12-14 01:22:31 +0900

    一旦コミット

--- php/mail.php
+++ php/mail.php
@@ -5,12 +5,11 @@ mb_internal_encoding("UTF-8");	//エンコードをUTF-8に
 $name = $_POST['name'];
 $mail = $_POST['mail'];
 $msg  = $_POST['msg'];
+$pfrom = "-f $mail";
 
-$to 		= "yuta.business01@gmail.com";
+$to 		= "yuta-abc.0720@ezweb.ne.jp";
 $subject 	= 'テストメール';
 $headers 	= 'From: $mail';
 
-if(!mb_send_mail($to, $subject, $msg, $headers)){
-	exit("遅れ中田よ");
-}
+mb_send_mail($to, $subject, $msg, $headers, $pfrom);
 ?>
